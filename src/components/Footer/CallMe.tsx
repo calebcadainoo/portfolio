de33dec67cdd70557158b775f5a2cc58768c8ebe
Caleb Crosby Adainoo <code.crosby@gmail.com>
2022-09-21 09:44:28 +0000

    feat: CallMe - designed component

--- src/components/Footer/CallMe.tsx
+++ src/components/Footer/CallMe.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { BioTitle } from 'components/Header';
+import UpRightIcon from 'assets/up-right-arrow.png';
 
 const CallMe = () => {
-	return <div className="centroid page-section under-section">CallMe</div>;
+	return (
+		<div className="centroid call-me-section page-section under-section">
+			<div className="call-me-circle">
+				<div>
+					<BioTitle text={'Describe your project'} />
+					<div className="call-me-text">
+						Call <br />
+						Crosby <br />
+						<img src={UpRightIcon} alt="call crosby" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CallMe;
